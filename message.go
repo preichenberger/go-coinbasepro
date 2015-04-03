48c1219e49d6912796027b88350f6b9dde31411a
Philip Reichenberger <preichenberger@philcolabs.com>
2015-04-03 15:09:35 -0400

    Added extra fields for message

--- message.go
+++ message.go
@@ -5,7 +5,12 @@ type Message struct {
   TradeId int `json:"trade_id,number"`
   OrderId string `json:"order_id"`
   Sequence int `json:"sequence,number"`
+  MakerOrderId string `json:"maker_order_id"`
+  TakerOrderId string `json:"taker_order_id"`
   Time Time `json:"time,string"`
+  RemainingSize float64 `json:"size,string"`
+  NewSize float64 `json:"new_size,string"`
+  OldSize float64 `json:"old_size,string"`
   Size float64 `json:"size,string"`
   Price float64 `json:"price,string"`
   Side string `json:"side"`
