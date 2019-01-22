a83d54da0604fd3a93a0bd6bb7cc4f8bec0faba8
Lukasz Raczylo <lukasz@raczylo.com>
2019-01-22 09:52:02 +0000

    Add withdrawal to coinbase account.

--- withdrawals.go
+++ withdrawals.go
@@ -10,10 +10,22 @@ type WithdrawalCrypto struct {
 	CryptoAddress string `json:"crypto_address"`
 }
 
+type WithdrawalCoinbase struct {
+	Currency          string `json:"currency"`
+	Amount            string `json:"amount"`
+	CoinbaseAccountId string `json:"coinbase_account_id"`
+}
+
 func (c *Client) CreateWithdrawalCrypto(newWithdrawCrypto *WithdrawalCrypto) (WithdrawalCrypto, error) {
 	var savedWithdrawal WithdrawalCrypto
-
 	url := fmt.Sprintf("/withdrawals/crypto")
 	_, err := c.Request("POST", url, newWithdrawCrypto, &savedWithdrawal)
+	return savedWithdrawal, err
+}
+
+func (c *Client) CreateWithdrawalCoinbase(newWithdrawCoinbase *WithdrawalCoinbase) (WithdrawalCoinbase, error) {
+	var savedWithdrawal WithdrawalCoinbase
+	url := fmt.Sprintf("/withdrawals/coinbase-account")
+	_, err := c.Request("POST", url, newWithdrawCoinbase, &savedWithdrawal)
 	return savedWithdrawal, err
 }
