3f6ee3cd8f1611991dcf4125058e30e902919f97
AlessandroSechi <alessandro.sechi@yahoo.it>
2021-06-04 11:12:48 +0200

    Added "trading_disabled" field in Get Products endpoint

--- product.go
+++ product.go
@@ -10,22 +10,23 @@ import (
 )
 
 type Product struct {
-	ID             string `json:"id"`
-	BaseCurrency   string `json:"base_currency"`
-	QuoteCurrency  string `json:"quote_currency"`
-	BaseMinSize    string `json:"base_min_size"`
-	BaseMaxSize    string `json:"base_max_size"`
-	QuoteIncrement string `json:"quote_increment"`
-	BaseIncrement  string `json:"base_increment"`
-	DisplayName    string `json:"display_name"`
-	MinMarketFunds string `json:"min_market_funds"`
-	MaxMarketFunds string `json:"max_market_funds"`
-	MarginEnabled  bool   `json:"margin_enabled"`
-	PostOnly       bool   `json:"post_only"`
-	LimitOnly      bool   `json:"limit_only"`
-	CancelOnly     bool   `json:"cancel_only"`
-	Status         string `json:"status"`
-	StatusMessage  string `json:"status_message"`
+	ID              string `json:"id"`
+	BaseCurrency    string `json:"base_currency"`
+	QuoteCurrency   string `json:"quote_currency"`
+	BaseMinSize     string `json:"base_min_size"`
+	BaseMaxSize     string `json:"base_max_size"`
+	QuoteIncrement  string `json:"quote_increment"`
+	BaseIncrement   string `json:"base_increment"`
+	DisplayName     string `json:"display_name"`
+	MinMarketFunds  string `json:"min_market_funds"`
+	MaxMarketFunds  string `json:"max_market_funds"`
+	MarginEnabled   bool   `json:"margin_enabled"`
+	PostOnly        bool   `json:"post_only"`
+	LimitOnly       bool   `json:"limit_only"`
+	CancelOnly      bool   `json:"cancel_only"`
+	TradingDisabled bool   `json:"trading_disabled"`
+	Status          string `json:"status"`
+	StatusMessage   string `json:"status_message"`
 }
 
 type Ticker struct {
